ccb85b2892daa4d9994ea7a690e2b24beaac0552
DanielVillatoro <danielvillatoroc@gmail.com>
2020-11-29 22:38:45 -0600

    Login finalizado

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputersDD.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult LoginUser(string username, string pass,string sede)
+        //{
+        //    LoginModel modeloLogin = new LoginModel();
+        //    DataTable dtResultado = modeloLogin.ValidacionLogin(username, pass);
+        //    if (dtResultado.Rows.Count > 0)
+        //    {
+        //        HttpContext.Session.SetString("Sede", dtResultado.Rows[0]["tipo"].ToString());
+        //        return RedirectToAction("Index", "Inicio");
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Index", "Login");
+        //    }
+        //}
+    }
+}
