108514e4d417515661a8350e2c43893c942abf10
DanielVillatoro <danielvillatoroc@gmail.com>
2020-12-02 23:17:33 -0600

    Modulo de categoria en vendedor finalizado

---
+++ Controllers/VendedorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputersDD.Controllers
+{
+    public class VendedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Categoria()
+        {
+            return View();
+        }
+        public IActionResult Producto()
+        {
+            return View();
+        }
+        public IActionResult Inventario()
+        {
+            return View();
+        }
+        public IActionResult Factura()
+        {
+            return View();
+        }
+    }
+}
